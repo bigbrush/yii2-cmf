911a4e5a644a6b4b5d351df1ecbe882f6f57be8e
philoweb <mj@bigbrush-agency.com>
2016-02-06 16:11:12 +0100

    Migration updated to include a config menu item

--- src/migrations/m150625_103538_create_admin_menu.php
+++ src/migrations/m150625_103538_create_admin_menu.php
@@ -12,7 +12,7 @@ class m150625_103538_create_admin_menu extends Migration
         $this->execute($sql);
 
         $this->batchInsert('{{%admin_menu}}', ['title', 'alias', 'route', 'state', 'tree', 'lft', 'rgt', 'depth', 'is_default', 'params'], [
-            ['Main menu', 'main-menu', '', 1, 1, 1, 30, 0, 0, ''],
+            ['Main menu', 'main-menu', '', 1, 1, 1, 32, 0, 0, ''],
             ['Home', 'home', '/', 1, 1, 2, 3, 1, 1, '{"icon":"home"}'],
             ['Content', 'content', '#', 1, 1, 4, 9, 1, 0, '{"icon":"paw"}'],
             ['Pages', 'pages', '/pages/page/index', 1, 1, 5, 6, 2, 0, '{"icon":"file"}'],
@@ -23,10 +23,11 @@ class m150625_103538_create_admin_menu extends Migration
             ['Blocks', 'blocks', '/big/block/index', 1, 1, 16, 17, 1, 0, '{"icon":"square"}'],
             ['File manager', 'file-manager', '/big/media/show', 1, 1, 18, 19, 1, 0, '{"icon":"picture-o"}'],
             ['Templates', 'templates', '/big/template/index', 1, 1, 20, 21, 1, 0, '{"icon":"simplybuilt"}'],
-            ['System', 'system', '#', 1, 1, 22, 29, 1, 0, '{"icon":"gears"}'],
+            ['System', 'system', '#', 1, 1, 22, 31, 1, 0, '{"icon":"gears"}'],
             ['Users', 'users', '/big/user/index', 1, 1, 23, 24, 2, 0, '{"icon":"users"}'],
             ['Extensions', 'extensions', '/big/extension/index', 1, 1, 25, 26, 2, 0, '{"icon":"plug"}'],
             ['Menu items', 'menus', '/big/admin-menu-item/index', 1, 1, 27, 28, 2, 0, '{"icon":"tree"}'],
+            ['Settings', 'settings', '/big/config/show?section=cms', 1, 1, 29, 30, 2, 0, '{"icon":"flag"}'],
         ]);
     }
 
