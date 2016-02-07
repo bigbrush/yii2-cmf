ccef8ee194bebe32039d996273f49a7561cfc6d7
philoweb <mj@bigbrush-agency.com>
2016-02-07 19:31:36 +0100

    Pages module: backend UI updated

--- src/modules/pages/backend/views/category/_tab_publishing.php
+++ src/modules/pages/backend/views/category/_tab_publishing.php
@@ -7,39 +7,47 @@
 ?>
 <div class="row">
     <div class="col-md-12">
-        <h3><?= Yii::t('cms', 'Category') ?></h3>
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'params[show_category_title]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show category title')) ?>
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'params[show_category_content]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show category content')) ?>
+        <div class="panel panel-default">
+            <div class="panel-heading"><?= Yii::t('cms', 'Category') ?></div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'params[show_category_title]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show category title')) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'params[show_category_content]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show category content')) ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="col-md-12">
-        <h3><?= Yii::t('cms', 'Pages') ?></h3>
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'params[pages_pr_row]')->dropDownList([
-                    1 => 1,
-                    2 => 2,
-                    3 => 3,
-                    4 => 4,
-                    6 => 6,
-                ])->label(Yii::t('cms', 'Pages pr row')) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'params[show_page_dates]')->dropDownList([
-                    0 => Yii::t('cms', 'No'),
-                    'created_at' => Yii::t('cms', 'Created'),
-                    'updated_at' => Yii::t('cms', 'Updated'),
-                ])->label(Yii::t('cms', 'Show page date')) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'params[show_page_content]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show page content')) ?>
+        <div class="panel panel-default">
+            <div class="panel-heading"><?= Yii::t('cms', 'Pages') ?></div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'params[pages_pr_row]')->dropDownList([
+                            1 => 1,
+                            2 => 2,
+                            3 => 3,
+                            4 => 4,
+                            6 => 6,
+                        ])->label(Yii::t('cms', 'Pages pr row')) ?>
+                    </div>
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'params[show_page_dates]')->dropDownList([
+                            0 => Yii::t('cms', 'No'),
+                            'created_at' => Yii::t('cms', 'Created'),
+                            'updated_at' => Yii::t('cms', 'Updated'),
+                        ])->label(Yii::t('cms', 'Show page date')) ?>
+                    </div>
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'params[show_page_content]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show page content')) ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
