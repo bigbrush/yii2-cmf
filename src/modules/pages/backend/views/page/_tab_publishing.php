ed316303ff401147a106e86e117c94496e3383d9
philoweb <mj@bigbrush-agency.com>
2016-02-18 12:18:42 +0100

    Removed option to show intro image - use gallery instead

--- src/modules/pages/backend/views/page/_tab_publishing.php
+++ src/modules/pages/backend/views/page/_tab_publishing.php
@@ -10,23 +10,17 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <?= $form->field($model, 'params[show_intro_content]')->dropDownList([Yii::t('cms', 'No'), Yii::t('cms', 'Yes')])->label(Yii::t('cms', 'Show page intro')) ?>
                     </div>
-                    <div class="col-md-3">
-                        <?= $form->field($model, 'params[show_intro_image]')->dropDownList([
-                            Yii::t('cms', 'No'),
-                            Yii::t('cms', 'Yes'),
-                        ])->label(Yii::t('cms', 'Show intro image')) ?>
-                    </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <?= $form->field($model, 'params[show_dates]')->dropDownList([
                             0 => Yii::t('cms', 'No'),
                             'created_at' => Yii::t('cms', 'Created'),
                             'updated_at' => Yii::t('cms', 'Updated'),
                         ])->label(Yii::t('cms', 'Show date')) ?>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <?= $form->field($model, 'params[show_editor_author]')->dropDownList([
                             0 => Yii::t('cms', 'No'),
                             'author' => Yii::t('cms', 'Author'),
